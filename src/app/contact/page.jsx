a2b8fe6c8a002d2ddd3ef0a86aeb4e9d2ee7e687
Easin Arafat <arafatgw5@gmail.com>
2026-06-30 19:31:49 +0600

    Resolve merge conflict

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -13,11 +13,7 @@ const infoData = [
   },
 ];
 
-<<<<<<< HEAD
- const ContactPage = () => {
-=======
 const ContactPage = () => {
->>>>>>> 5ecbec057e38bb4142a7f973d49e609781513972
   return (
     <Container className="py-6 md:py-12">
       <div className="flex flex-col md:flex-row gap-6 md:gap-14">
@@ -27,7 +23,6 @@ const ContactPage = () => {
         <div className="w-full md:w-1/3 flex flex-col justify-center gap-4 md:gap-8">
           {infoData?.map((item) => (
             <div key={item?.title} className="flex items-center space-x-4">
-<<<<<<< HEAD
               <span className="rounded-md bg-[var(--color-primary)]/10 p-4 text-[var(--color-primary)]">
                 {item?.icon}
               </span>
@@ -36,14 +31,6 @@ const ContactPage = () => {
                   {item?.title}
                 </h3>
                 <p className="text-[var(--color-text)]">{item?.description}</p>
-=======
-              <span className="bg-lightSky/5 p-4 rounded-md">{item?.icon}</span>
-              <div>
-                <h3 className="text-white/60 text-sm font-semibold">
-                  {item?.title}
-                </h3>
-                <p>{item?.description}</p>
->>>>>>> 5ecbec057e38bb4142a7f973d49e609781513972
               </div>
             </div>
           ))}
@@ -53,8 +40,4 @@ const ContactPage = () => {
   );
 };
 
-<<<<<<< HEAD
 export default ContactPage;
-=======
-export default ContactPage;
->>>>>>> 5ecbec057e38bb4142a7f973d49e609781513972
